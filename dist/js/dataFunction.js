c7b2e653b7b7297dccd1cdf92eeb4504ac57d333
Darkseid <khoinguyendao1210@gmail.com>
2023-05-24 09:38:31 +0700

    f

--- dist/js/dataFunction.js
+++ dist/js/dataFunction.js
@@ -40,7 +40,7 @@ export const getWeatherFromCoords = async(locationObj)=>{
         console.log(weatherJson)
         return weatherJson;
     }catch(err){
-        console.error(err);
+        console.log(err);
     }
 };
 
